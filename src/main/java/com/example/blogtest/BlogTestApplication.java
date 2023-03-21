f8c9e237030e2fc0f57026face3a819ffb0998c1
yongjae_park <ytothej92@gmail.com>
2023-03-21 10:53:58 +0900

    refactor: @EnableCachIng 어노테이션 추가

--- src/main/java/com/example/blogtest/BlogTestApplication.java
+++ src/main/java/com/example/blogtest/BlogTestApplication.java
@@ -2,12 +2,13 @@ package com.example.blogtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BlogTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogTestApplication.class, args);
 	}
-
 }
